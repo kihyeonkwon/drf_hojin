b133a7b8e6343d03d851e633db661d869927344b
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2023-09-26 19:49:44 +0900

    made post article endpoint

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import ArticleView
 
 urlpatterns = [
-    path('', ArticleView.as_view())
+    path('', ArticleView.as_view()),
 ]
--- articles/views.py
+++ articles/views.py
@@ -9,4 +9,12 @@ class ArticleView(APIView):
     def get(self, request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)    
+        return Response(serializer.data)
+    
+    def post(self, request):
+        print(request.data)
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
