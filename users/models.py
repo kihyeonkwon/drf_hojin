ebe2f004676010faeeba753b9605399213005cda
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2023-10-04 20:21:52 +0900

    make user manager

--- users/models.py
+++ users/models.py
@@ -1,5 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_admin=False, is_staff=False, is_active=True):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            password=password,
+            is_admin=is_admin,
+            is_staff=is_staff,
+            is_active=is_active,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_admin=True,
+        )
+        # user.save(using=self._db)
+        return user
+
+
+
 
 
 class MyUser(AbstractBaseUser):
@@ -13,8 +45,27 @@ class MyUser(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
 
-    REQUIRED_FIELDS = [ 'name', 'password']
+    REQUIRED_FIELDS = []
+
+    objects = MyUserManager()
 
     def __str__(self):
         return self.email
+    
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
 
