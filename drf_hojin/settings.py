a1faf03378c83e035526f274ddb24624036789a4
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2023-10-04 19:45:04 +0900

    create users app

--- drf_hojin/settings.py
+++ drf_hojin/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'articles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",
+
+]
+
 
 ROOT_URLCONF = 'drf_hojin.urls'
 
