87c1a286834c97e94517dfb506855c82feee221c
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2023-10-04 19:56:42 +0900

    make user model

--- drf_hojin/settings.py
+++ drf_hojin/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'articles',
+    'users',
     'corsheaders',
 ]
 
@@ -131,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "users.MyUser"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = [ 'name', 'password']
+
+    def __str__(self):
+        return self.email
+
