f1637f176a05a76065f0d2e4ce257d0b8c49c3f0
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2023-10-05 20:13:43 +0900

    add simplejwt dependency

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+# from . import views
+from .views import UserView
+
+urlpatterns = [
+    path('', UserView.as_view()),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 # Create your views here.
+
+
+
+# 회원가입을 위한 class형 뷰 작성할 예정
+
+class UserView(APIView):
+    print("UserView")
+    pass
